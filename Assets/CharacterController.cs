39ad2c0eba9e7da2d6359a6685eff69aee91ddc4
Jan de Wit <jan@neryana.com>
2019-06-10 22:10:37 +0200

    Initial commit

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    // Basic first person camera/controls: https://www.youtube.com/watch?v=blO039OzUZc
+
+    public float speed = 10.0F;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float strafe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        strafe *= Time.deltaTime;
+
+        transform.Translate(strafe, 0, translation);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }	
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
+        }
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # UnityPixelated3DDemo
 Pixelated 3D look in Unity3D (using RenderTexture)
+
+![Screenshot of the Unity3D pixelated 3D demo](Screenshots/screenshot.png)
+
+Made in Unity 5.6.0f3.
+This is an implementation of [this tutorial by Iain Dowling on how to get this pixelated effect using render textures](https://iaindowling.weebly.com/blog/unity-pixelated-render-effect) -- I thought it would be nice to have a working implementation as a starting point.
+
+The demo environment mesh was made using [Asset Forge](http://assetforge.io/) (quickly thrown together, it does not nearly showcase all of the Asset Forge features)
